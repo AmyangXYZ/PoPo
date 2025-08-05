9f710601d5f76a9c137c827e6f638273f44f12a1
AmyangXYZ <amyang.xyz@gmail.com>
2025-08-05 11:59:09 -0400

    add labeling scene

---
+++ src/components/labeling-scene.tsx
@@ -0,0 +1,330 @@
+"use client"
+
+import {
+  ArcRotateCamera,
+  Camera,
+  Color3,
+  Color4,
+  CreateScreenshotAsync,
+  DirectionalLight,
+  Engine,
+  HemisphericLight,
+  LoadAssetContainerAsync,
+  Material,
+  Mesh,
+  RegisterSceneLoaderPlugin,
+  Scene,
+  Vector3,
+} from "@babylonjs/core"
+import { useRef, useEffect, useCallback, useState } from "react"
+import {
+  MmdWasmModel,
+  SdefInjector,
+  MmdWasmInstanceTypeMPR,
+  GetMmdWasmInstance,
+  MmdWasmRuntime,
+  MmdWasmPhysics,
+  type IMmdWasmInstance,
+  MmdStandardMaterialBuilder,
+  MmdStandardMaterial,
+  VpdLoader,
+  BpmxLoader,
+  VmdLoader,
+} from "babylon-mmd"
+
+import { MPLBoneFrame, Quaternion as MPLQuaternion, Vector3 as MPLVector3 } from "mmd-mpl"
+import { useMPLCompiler } from "@/hooks/useMPLCompiler"
+
+export default function LabelingScene() {
+  const mplCompiler = useMPLCompiler()
+
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const engineRef = useRef<Engine>(null)
+  const sceneRef = useRef<Scene>(null)
+  const cameraRef = useRef<Camera>(null)
+  const mmdWasmInstanceRef = useRef<IMmdWasmInstance>(null)
+  const mmdRuntimeRef = useRef<MmdWasmRuntime>(null)
+  const mmdMaterialBuilderRef = useRef<MmdStandardMaterialBuilder>(null)
+  const vpdLoaderRef = useRef<VpdLoader>(null)
+  const vmdLoaderRef = useRef<VmdLoader>(null)
+  const modelRef = useRef<MmdWasmModel>(null)
+
+  const [currentVpd, setCurrentVpd] = useState<string>("")
+
+  const modelNameRef = useRef(localStorage.getItem("selectedModel") || "深空之眼-梵天")
+  const lastVMDUrlRef = useRef("")
+
+  const dataRef = useRef<{
+    vpd: string
+    mpl: string
+    image: string
+  }[]>([])
+
+
+  const [modelLoaded, setModelLoaded] = useState(false)
+
+  const loadVMD = useCallback(
+    async (vmdUrl: string) => {
+      if (!vmdLoaderRef.current || !modelRef.current || !mplCompiler) return null
+      lastVMDUrlRef.current = vmdUrl
+      if (vmdUrl === "") {
+        modelRef.current.removeAnimation(0)
+        return
+      }
+      const vmd = await vmdLoaderRef.current.loadAsync("vmd_animation", vmdUrl)
+      modelRef.current.addAnimation(vmd)
+      modelRef.current.setAnimation("vmd_animation")
+      mmdRuntimeRef.current!.seekAnimation(0, true)
+      mmdRuntimeRef.current!.playAnimation()
+    },
+    [vmdLoaderRef, modelRef, mplCompiler]
+  )
+
+  const loadModel = useCallback(async (): Promise<void> => {
+    if (!sceneRef.current || !mmdWasmInstanceRef.current || !mmdRuntimeRef.current || !modelNameRef.current) return
+    if (modelRef.current) {
+      mmdRuntimeRef.current.destroyMmdModel(modelRef.current)
+      modelRef.current.mesh.dispose()
+    }
+
+    LoadAssetContainerAsync(`/models/${modelNameRef.current}.bpmx`, sceneRef.current!, {
+      pluginOptions: {
+        mmdmodel: {
+          materialBuilder: mmdMaterialBuilderRef.current || undefined,
+        },
+      },
+    }).then(async (result) => {
+      const mesh = result.meshes[0]
+      modelRef.current = mmdRuntimeRef.current!.createMmdModel(mesh as Mesh, {
+        buildPhysics: {
+          disableOffsetForConstraintFrame: true,
+        },
+      })
+
+      result.addAllToScene()
+      setModelLoaded(true)
+
+      if (lastVMDUrlRef.current !== "") {
+        loadVMD(lastVMDUrlRef.current)
+      }
+    })
+  }, [loadVMD])
+
+  const loadVPD = useCallback(
+    async (vpdUrl: string): Promise<MPLBoneFrame[] | null> => {
+      if (!vpdLoaderRef.current || !modelRef.current || !mplCompiler) return null
+
+      const vpd = await vpdLoaderRef.current.loadAsync("vpd_pose", vpdUrl)
+      // modelRef.current.addAnimation(vpd)
+      // modelRef.current.setAnimation("vpd_pose")
+      // modelRef.current.currentAnimation?.animate(0)
+      const boneStates: MPLBoneFrame[] = []
+      for (const boneTrack of vpd.boneTracks) {
+        const boneNameJp = boneTrack.name
+        const boneNameEn = mplCompiler.get_bone_english_name(boneNameJp)
+        if (!boneNameEn) {
+          continue
+        }
+
+        const rotation = boneTrack.rotations
+        if (rotation.length === 0) continue
+
+        if (!(rotation[0] === 0 && rotation[1] === 0 && rotation[2] === 0 && rotation[3] === 1)) {
+          boneStates.push(
+            new MPLBoneFrame(
+              boneNameEn,
+              boneNameJp,
+              new MPLVector3(0, 0, 0),
+              new MPLQuaternion(rotation[0], rotation[1], rotation[2], rotation[3])
+            )
+          )
+        }
+      }
+
+      for (const boneTrack of vpd.movableBoneTracks) {
+        const boneNameJp = boneTrack.name
+        const boneNameEn = mplCompiler.get_bone_english_name(boneNameJp)
+        if (!boneNameEn) {
+          continue
+        }
+        let position = new MPLVector3(0, 0, 0)
+        let rotation = new MPLQuaternion(0, 0, 0, 1)
+        if (boneTrack.positions && boneTrack.positions.length > 0) {
+          position = new MPLVector3(boneTrack.positions[0], boneTrack.positions[1], boneTrack.positions[2])
+        }
+
+        if (boneTrack.rotations && boneTrack.rotations.length > 0) {
+          rotation = new MPLQuaternion(
+            boneTrack.rotations[0],
+            boneTrack.rotations[1],
+            boneTrack.rotations[2],
+            boneTrack.rotations[3]
+          )
+        }
+        boneStates.push(new MPLBoneFrame(boneNameEn, boneNameJp, position, rotation))
+      }
+      return boneStates
+    },
+    [vpdLoaderRef, modelRef, mplCompiler]
+  )
+
+  useEffect(() => {
+    const resize = () => {
+      if (sceneRef.current) {
+        sceneRef.current.getEngine().resize()
+      }
+    }
+
+    const init = async () => {
+      if (!canvasRef.current || !mplCompiler) return
+
+      // Register the PMX loader plugin
+      RegisterSceneLoaderPlugin(new BpmxLoader())
+
+      const engine = new Engine(canvasRef.current, true, {}, true)
+      SdefInjector.OverrideEngineCreateEffect(engine)
+
+      const scene = new Scene(engine)
+
+      scene.clearColor = new Color4(1, 1, 1, 1)
+
+      engineRef.current = engine
+      sceneRef.current = scene
+
+      const camera = new ArcRotateCamera("ArcRotateCamera", 0, 0, 45, new Vector3(0, 12, 0), scene)
+      camera.setPosition(new Vector3(0, 20, -30))
+      camera.attachControl(canvasRef.current, false)
+      camera.inertia = 0.8
+      camera.speed = 10
+      camera.radius = 40
+      cameraRef.current = camera
+
+      scene.activeCameras = [camera]
+
+      const hemisphericLight = new HemisphericLight("hemisphericLight", new Vector3(0, 1, 0), scene)
+      hemisphericLight.intensity = 0.5
+      hemisphericLight.specular = new Color3(0, 0, 0)
+      hemisphericLight.groundColor = new Color3(1, 1, 1)
+
+      const directionalLight = new DirectionalLight("directionalLight", new Vector3(0.5, -1, 1), scene)
+      directionalLight.intensity = 0.5
+      directionalLight.autoCalcShadowZBounds = false
+      directionalLight.autoUpdateExtends = false
+      directionalLight.shadowMaxZ = 20 * 3
+      directionalLight.shadowMinZ = -30
+      directionalLight.orthoTop = 18 * 3
+      directionalLight.orthoBottom = -1 * 3
+      directionalLight.orthoLeft = -10 * 3
+      directionalLight.orthoRight = 10 * 3
+      directionalLight.shadowOrthoScale = 0
+
+      mmdWasmInstanceRef.current = await GetMmdWasmInstance(new MmdWasmInstanceTypeMPR())
+      const mmdRuntime = new MmdWasmRuntime(mmdWasmInstanceRef.current, scene, new MmdWasmPhysics(scene))
+      mmdRuntime.register(scene)
+      mmdRuntimeRef.current = mmdRuntime
+
+
+
+      const materialBuilder = new MmdStandardMaterialBuilder()
+      //   materialBuilder.loadOutlineRenderingProperties = (): void => {
+      //     /* do nothing */
+      //   }
+      materialBuilder.afterBuildSingleMaterial = (material: MmdStandardMaterial): void => {
+        material.forceDepthWrite = true
+        material.useAlphaFromDiffuseTexture = true
+        material.specularColor = new Color3(0, 0, 0)
+        if (material.diffuseTexture !== null) material.diffuseTexture.hasAlpha = true
+
+        if (material.transparencyMode === Material.MATERIAL_ALPHABLEND) {
+          material.transparencyMode = Material.MATERIAL_ALPHATESTANDBLEND
+          material.alphaCutOff = 0.01
+        }
+      }
+      mmdMaterialBuilderRef.current = materialBuilder
+
+      vpdLoaderRef.current = new VpdLoader(scene)
+      vmdLoaderRef.current = new VmdLoader(scene)
+
+      loadModel()
+
+      window.addEventListener("resize", resize)
+
+      engine.runRenderLoop(() => {
+        scene.render()
+      })
+    }
+    init()
+
+    return () => {
+      if (engineRef.current) {
+        engineRef.current.dispose()
+        window.removeEventListener("resize", resize)
+      }
+    }
+  }, [loadModel, mplCompiler])
+
+  const takeScreenshot = useCallback(async (): Promise<string> => {
+    if (!canvasRef.current || !engineRef.current || !cameraRef.current) return ""
+    const b64 = await CreateScreenshotAsync(engineRef.current!, cameraRef.current!, { precision: 1 })
+    return b64
+  }, [canvasRef])
+
+  const generateData = useCallback(async (url: string): Promise<{ vpd: string, description: string, mpl: string, image: string } | null> => {
+    if (!mplCompiler || !modelLoaded) return null
+    const boneStates = await loadVPD(url)
+    if (!boneStates) {
+      return null
+    }
+    const statements = mplCompiler.reverse_compile("vpd_pose", boneStates)
+    const vmdBytes = mplCompiler.compile(statements)
+    if (vmdBytes.length === 0) {
+      loadVMD("")
+      return null
+    }
+
+    const vmdBlob = new Blob([vmdBytes], { type: "application/octet-stream" })
+    const vmdUrl = URL.createObjectURL(vmdBlob)
+    loadVMD(vmdUrl)
+    const b64 = await takeScreenshot()
+    URL.revokeObjectURL(vmdUrl)
+    return {
+      vpd: url,
+      description: "",
+      mpl: statements,
+      image: b64
+    }
+  }, [mplCompiler, modelLoaded, loadVPD, loadVMD, takeScreenshot])
+
+  useEffect(() => {
+    const load = async () => {
+      if (modelLoaded && mplCompiler) {
+        const start = 1
+        const end = 1
+        for (let i = start; i <= end; i++) {
+          setCurrentVpd(`${i}.vpd`)
+          const data = await generateData(`/vpd/${i}.vpd`)
+          if (data) {
+            dataRef.current.push(data)
+          }
+        }
+        const json = JSON.stringify(dataRef.current)
+        const blob = new Blob([json], { type: "application/json" })
+        const url = URL.createObjectURL(blob)
+        const link = document.createElement("a")
+        link.href = url
+        link.download = `pose_data_${start}_${end}.json`
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+      }
+    }
+    load()
+  }, [modelLoaded, mplCompiler, generateData])
+
+  return (
+    <div className="w-full h-full">
+      <div className="text-center">{currentVpd}</div>
+      <canvas ref={canvasRef} className="w-[400px] h-[600px] mx-auto z-1 outline-none" />
+    </div>
+  )
+}
